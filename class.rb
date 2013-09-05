c3ec8663f9173266cff66b15422a5154ea37c32b
MakerSquare Student <students@themakersquare.com>
2013-09-05 15:53:38 -0500

    Make first version of class

---
+++ class.rb
@@ -0,0 +1,11 @@
+5.send(:*,5)
+"omg".send(:upcase)
+['a','b','c'].send(:at,1)
+['a','b','c'].send(:insert,2,'o','h','n','o')
+{}.send(:size)
+{character: "Mario"}.send(:has_key?,:character)
+6-32
+{html:true, json:false}.keys
+"MakerSquare"*6
+"MakerSquare".split('a')
+['alpha','beta'][1]
